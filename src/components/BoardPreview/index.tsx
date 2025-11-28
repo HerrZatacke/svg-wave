3323ca3bb3a5e3ba27c6aed121c8ba680ac2cc00
Andreas Hahn <zatacke@gmail.com>
2025-11-28 16:47:10 +0100

    Added pinch-zoom for board preview

--- src/components/BoardPreview/index.tsx
+++ src/components/BoardPreview/index.tsx
@@ -1,4 +1,5 @@
 import './index.scss';
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import useBoardStore from '@/stores/boardStore';
 import useWavesStore from '@/stores/waveStore';
 import { boardDrill } from '@/tools/boardDrill';
@@ -17,59 +18,70 @@ export const BoardPreview: React.FC = () => {
 
   return (
     <div className="board-preview">
-      <svg
-        className="board-preview__svg"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox={`${Math.floor(width * -0.55)} ${Math.floor(height * -0.55)} ${Math.ceil(width * 1.1)} ${Math.ceil(height * 1.1)}`}
+      <TransformWrapper
+        initialScale={1}
+        minScale={1}
+        maxScale={10}
+        limitToBounds
+        centerZoomedOut
+        centerOnInit
       >
-        <polyline
-          className="board-preview__path board-preview__path--board"
-          points={toBoardOutline(width, height).map(({ x, y }) => `${x.toFixed(3)},${y.toFixed(3)}`).join(' ')}
-        />
-        {
-          holes.map(({ x, y, radius }, index) => (
-            <circle
-              className="board-preview__circle"
-              key={index}
-              cx={x}
-              cy={y}
-              r={radius}
+        <TransformComponent>
+          <svg
+            className="board-preview__svg"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={`${Math.floor(width * -0.55)} ${Math.floor(height * -0.55)} ${Math.ceil(width * 1.1)} ${Math.ceil(height * 1.1)}`}
+          >
+            <polyline
+              className="board-preview__path board-preview__path--board"
+              points={toBoardOutline(width, height).map(({ x, y }) => `${x.toFixed(3)},${y.toFixed(3)}`).join(' ')}
             />
-          ))
-        }
-        {
-          waves.map((wave) => {
-            const wavePaths = toCircularPath(wave, gapSize);
+            {
+              holes.map(({ x, y, radius }, index) => (
+                <circle
+                  className="board-preview__circle"
+                  key={index}
+                  cx={x}
+                  cy={y}
+                  r={radius}
+                />
+              ))
+            }
+            {
+              waves.map((wave) => {
+                const wavePaths = toCircularPath(wave, gapSize);
 
-            return wavePaths ? (
-              <g key={wave.id} id={wave.id}>
-                { paths ? (
-                  <>
-                    <polyline
-                      className="board-preview__path board-preview__path--inner"
-                      points={(wavePaths.pointsInner || []).map(({ x, y }) => `${x.toFixed(3)},${y.toFixed(3)}`).join(' ')}
-                    />
-                    <polyline
-                      className="board-preview__path board-preview__path--outer"
-                      points={(wavePaths.pointsOuter || []).map(({ x, y }) => `${x.toFixed(3)},${y.toFixed(3)}`).join(' ')}
-                    />
-                  </>
-                ) : (
-                  wavePaths.points.map(({ x, y }, index) => (
-                    <circle
-                      // className="board-preview__path board-preview__circle"
-                      key={index}
-                      cx={x.toFixed(3)}
-                      cy={y.toFixed(3)}
-                      r="0.5"
-                    />
-                  ))
-                )}
-              </g>
-            ) : null;
-          })
-        }
-      </svg>
+                return wavePaths ? (
+                  <g key={wave.id} id={wave.id}>
+                    { paths ? (
+                      <>
+                        <polyline
+                          className="board-preview__path board-preview__path--inner"
+                          points={(wavePaths.pointsInner || []).map(({ x, y }) => `${x.toFixed(3)},${y.toFixed(3)}`).join(' ')}
+                        />
+                        <polyline
+                          className="board-preview__path board-preview__path--outer"
+                          points={(wavePaths.pointsOuter || []).map(({ x, y }) => `${x.toFixed(3)},${y.toFixed(3)}`).join(' ')}
+                        />
+                      </>
+                    ) : (
+                      wavePaths.points.map(({ x, y }, index) => (
+                        <circle
+                          // className="board-preview__path board-preview__circle"
+                          key={index}
+                          cx={x.toFixed(3)}
+                          cy={y.toFixed(3)}
+                          r="0.5"
+                        />
+                      ))
+                    )}
+                  </g>
+                ) : null;
+              })
+            }
+          </svg>
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 };
