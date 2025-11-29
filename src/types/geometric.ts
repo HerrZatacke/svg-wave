ca97886c7fcea4fba9484ac25983a942e39bb851
Andreas Hahn <zatacke@gmail.com>
2025-11-29 18:45:24 +0100

    Formatting

--- src/types/geometric.ts
+++ src/types/geometric.ts
@@ -1,21 +1,21 @@
 export interface Point {
-  x: number,
-  y: number,
+  x: number;
+  y: number;
 }
 
-export type Line = [Point, Point]
+export type Line = [Point, Point];
 
 export interface WavePaths {
-  points: Point[],
-  pointsInner: Point[],
-  pointsOuter: Point[],
+  points: Point[];
+  pointsInner: Point[];
+  pointsOuter: Point[];
 }
 
 export interface CopperPaths {
-  pointsInner: Point[],
-  pointsOuter: Point[],
+  pointsInner: Point[];
+  pointsOuter: Point[];
 }
 
 export interface DrillHole extends Point {
-  radius: number,
+  radius: number;
 }
