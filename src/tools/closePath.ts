d433d4895b7212d7d4056952b37586008c35e8cd
Andreas Hahn <zatacke@gmail.com>
2025-11-29 17:39:43 +0100

    Added more checks for closePath.ts

--- src/tools/closePath.ts
+++ src/tools/closePath.ts
@@ -1,12 +1,22 @@
 import type { Point } from '@/types/geometric';
 
 export const closePath = (points: Point[]): Point[] => {
-  if (points.length < 1) {
+  if (points.length < 3) {
     return [];
+  }
+
+  const firstPoint = points[0];
+  const lastPoint = points[points.length - 1];
+
+  if (
+    firstPoint.x === lastPoint.x &&
+    firstPoint.y === lastPoint.y
+  ) {
+    return points;
   }
 
   return [
     ...points,
-    points[0],
+    firstPoint,
   ];
 };
